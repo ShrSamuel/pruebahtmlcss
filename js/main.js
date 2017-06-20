88fbb803ec217720501da4aed68aadb1c767200c
ShrSamuel <Shrsamuelherrera@gmail.com>
2017-06-20 17:08:28 +0200

    ???

--- js/main.js
+++ js/main.js
@@ -5,11 +5,11 @@ var myGameArea =
                         document.createElement("canvas"),
                         start : function()
                         {
-                            this.canvas.width = 480;
+                            this.canvas.width = 200;
                             this.canvas.height = 480;
                             this.context = this.canvas.getContext("2d");
-                                document.body.insertBefore(this.canvas,
-                                                           document.body.childNodes[0]);
+                            document.getElementById("zonaJuego").insertBefore(this.canvas,
+                                                           document.getElementById("zonaJuego").childNodes[0]);
                             this.frameNo=0;
                             this.interval= setInterval(updateGameArea,20);
                         
@@ -28,11 +28,7 @@ var myGameArea =
 
 
 
-function startGame()
-{
-    myGamePiece = new component(30,30,"./images/emoticono.png",10,120,"image");
-    myGameArea.start();
-}
+
 function component(width,height,color,x,y,type)
 {
     this.type=type;
@@ -74,19 +70,19 @@ function updateGameArea()
 }
 
 function moveup() {
-    myGamePiece.speedY = -1; 
+    myGamePiece.speedY = -5; 
 }
 
 function movedown() {
-    myGamePiece.speedY = 1; 
+    myGamePiece.speedY = 5; 
 }
 
 function moveleft() {
-    myGamePiece.speedX = -1; 
+    myGamePiece.speedX = -5; 
 }
 
 function moveright() {
-    myGamePiece.speedX = 1; 
+    myGamePiece.speedX = 5; 
 }
 function clearmove() {
     myGamePiece.speedX = 0; 
